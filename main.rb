6895d2cde1a88fc7e802be9cf746d52fdaf312bb
westnorthern <rrichardson610@gmail.com>
2018-01-29 01:10:36 -0700

    basic rool test functionality

--- main.rb
+++ main.rb
@@ -6,8 +6,34 @@ require 'json'
 
 set :database, "sqlite3:main.sqlite3"
 
+@rool_array = ['basic', 'all', 'blank', 'email', 'greater_than', 'less_than', 'equal']
+
 def symbol_hashilate(my_hash = {})
 	my_hash = my_hash.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+end
+
+def bad_hack(string)
+	if string == 'basic'
+		return Rool::Basic
+	end
+	if string == 'all'
+		return Rool::All
+	end
+	if string == 'blank'
+		return Rool::Blank
+	end
+	if string == 'email'
+		return Rool::Email
+	end
+	if string == 'greater_than'
+		return Rool::GreaterThan
+	end
+	if string == 'less_than'
+		return Rool::LessThan
+	end
+	if string == 'equal'
+		return Rool::Equal
+	end
 end
 
 def display_rool(hash, iteration = 0)
@@ -45,23 +71,23 @@ end
 
 get '/' do
 	@fullhash = Hash.new
-	@data = {foo: 12, bar: [1, 2, 3, 4, 5]}
-	@nested_rule = Rool::All.new(Rool::Equal.new(:foo, 10), Rool::Send.new(:bar, 30),Rool::All.new(Rool::Equal.new(:foo, 10), Rool::Send.new(:bar, 30)))
-	@nested_rule.process(@data)
-	@nested_rule_json = JSON.parse(@nested_rule.to_json)
+	@rool_array = ['basic', 'all', 'blank', 'email', 'greater_than', 'less_than', 'equal']
+	# @data = {foo: 12, bar: [1, 2, 3, 4, 5]}
+	# @nested_rule = Rool::All.new(Rool::Equal.new(:foo, 10), Rool::Send.new(:bar, 30),Rool::All.new(Rool::Equal.new(:foo, 10), Rool::Send.new(:bar, 30)))
+	# @nested_rule.process(@data)
+	# @nested_rule_json = JSON.parse(@nested_rule.to_json)
 
-	@test_raw_json = JSON.pretty_generate(@nested_rule_json)
+	# @test_raw_json = JSON.pretty_generate(@nested_rule_json)
 
-	@test_type_of = @nested_rule.class.to_s.split('::')[1].strip
+	# @test_type_of = @nested_rule.class.to_s.split('::')[1].strip
 
-	@test_display = display_rool(@nested_rule_json)
+	# @test_display = display_rool(@nested_rule_json)
 
-	# @test_three = display_rool(@test)
-	puts @test
 	erb :home
 end
 
 post '/add_to_hash' do
+	@rool_array = ['basic', 'all', 'blank', 'email', 'greater_than', 'less_than', 'equal']
 	@key = params[:hashkey]
 	@value = params[:hashvalue]
 	if @value.to_i.to_s == @value
@@ -70,14 +96,45 @@ post '/add_to_hash' do
 	@old_hash = eval(params[:fullhash])
 	@new_hash = @old_hash.merge!(@key => @value)
 	@fullhash = symbol_hashilate(@new_hash)
+	@key_array = @fullhash.keys
 
 	erb :home
 end
 
 post '/run_rool' do
+	@rool_array = ['basic', 'all', 'blank', 'email', 'greater_than', 'less_than', 'equal']
 	@fullhash = eval(params[:fullhash])
+	puts "this is the parsed hash"
+	p @fullhash
+	@key_array = @fullhash.keys
+	@rool = params[:rool]
+	@operand = params[:operand]
+	if @operand.to_i.to_s == @operand
+		@operand = @operand.to_i
+	end
+	@hash_key = params[:hash_key]
+	@rool_obj = bad_hack(@rool).new(@hash_key.to_sym, @operand)
+	p @rool_obj
+	@rool_obj.process(@fullhash)
+	@against = Rool::Equal.new(@hash_key, 12)
+	puts "did this change?"
+	p @rool_obj
+	@result = display_rool(JSON.parse(@rool_obj.to_json))
 
 	erb :home
 end
+
+post '/email_rool' do
+	@fullhash = eval(params[:fullhash])
+	@hash_key = params[:hash_key]
+	@rool = Rool::Email.new(@hash_key)
+	p @rool
+	@rool.process(@fullhash)
+	@result = display_rool(JSON.parse(@rool.to_json))
+
+	erb :home
+end
+
+
 		
 	
